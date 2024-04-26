413a0e243c50e2656479e1f682d9b1f2c38dc48d
VictorClever03 <clevervictor03@gmail.com>
2024-04-26 23:13:23 +0100

    adicionei os valores do income e outcome no useform, e fiz os comentarios

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,74 +1,115 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay, TransactionTypeButton, TransactioType } from "./styles";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionTypeButton,
+  TransactioType,
+} from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
-import * as z from 'zod';
-import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-// criei o esquema com nome dos campos dos inputs
+//1 useform criei o esquema com nome dos campos dos inputs
 const NewTransactionFormSchema = z.object({
   description: z.string(),
   price: z.number(),
   category: z.string(),
-  // type: z.enum(['income', 'outcome']),
-})
+  type: z.enum(["income", "outcome"]),
+});
 
-// peguei os inputs e lhes tipei conforme o esquema
-type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>; 
+//2 useform peguei os inputs e lhes tipei conforme o esquema
+type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>;
 export function NewTransactionModal() {
-// necessario que seja assim , ATT: controlar onde colocar os inputs e o schema
+  //3 useform necessario que seja assim , ATT: controlar onde colocar os inputs e o schema
   const {
-    register, 
+    // 8 useform, adicionei o control para dados que nao vem direcatamente do input ou outro formElement como checkbox, textarea etc..
+    control,
+    register,
     handleSubmit,
-    formState:{isSubmitting}
+    formState: { isSubmitting },
   } = useForm<NewTransactionFormInputs>({
     resolver: zodResolver(NewTransactionFormSchema),
+    // 10 useform para colocar certos campos do passo 1 com valores padraos
+    defaultValues:{
+      type: "income",
+    }
   });
 
-  // essa e a funcao que vai executar quando for submeter o formulario, ATT tipar data
+  //4 useform essa e a funcao que vai executar quando for submeter o formulario, ATT tipar data
   async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     console.log(data);
   }
 
   return (
     <Dialog.Portal>
-      <Overlay/>
+      <Overlay />
 
       <Content>
         <Dialog.Title>Nova Transacao </Dialog.Title>
-        <CloseButton >
-            <X size={24} />
+        <CloseButton>
+          <X size={24} />
         </CloseButton>
 
-
+        {/* 5 useform handleSubmit(handleCreateNewTransaction) */}
         <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
-            <input type="text"  placeholder="Descricao" required
-            {...register('description')}
-            />
-            {/* no zodschema coloquei o price como number e aqui sem o valueAsNumber vai retornar como string dando erro */}
-            <input type="number"  placeholder="Preco" required
-            {...register('price', {valueAsNumber:true})}
-            />
-            <input type="text" placeholder="Categoria" required
-            {...register('category')}
-            />
-
-            <TransactioType>
-                <TransactionTypeButton variant="income" value="income" title="clique">
+          <input
+            type="text"
+            placeholder="Descricao"
+            required
+            {...register("description")}
+          />
+          {/* 6 useform no zodschema coloquei o price como number e aqui sem o valueAsNumber vai retornar como string dando erro , {...register('category')} no input*/}
+          <input
+            type="number"
+            placeholder="Preco"
+            required
+            {...register("price", { valueAsNumber: true })}
+          />
+          <input
+            type="text"
+            placeholder="Categoria"
+            required
+            {...register("category")}
+          />
+          {/* 9 useform o control e para dados que nao virao directamente do input ou outro formElement */}
+          <Controller
+            control={control}
+            name="type"
+            render={({ field }) => {
+              // console.log(field)
+              return (
+                <TransactioType onValueChange={field.onChange} value={field.value}>
+                  <TransactionTypeButton
+                    variant="income"
+                    value="income"
+                    title="clique"
+                  >
                     <ArrowCircleUp size={24} />
                     Entrada
-                </TransactionTypeButton>
-                
-                <TransactionTypeButton variant="outcome" value="outcome" title="clique">
+                  </TransactionTypeButton>
+
+                  <TransactionTypeButton
+                    variant="outcome"
+                    value="outcome"
+                    title="clique"
+                  >
                     <ArrowCircleDown size={24} />
                     Saida
-                </TransactionTypeButton>
-            </TransactioType>
-            {/* desativa o botao quando estiver no processo de submissao */}
-            <button type="submit" disabled={isSubmitting}> Cadastrar </button>
-        </form>
+                  </TransactionTypeButton>
+                </TransactioType>
+              );
+            }}
+          />
 
+          {/*7 useform desativa o botao quando estiver no processo de submissao */}
+          <button type="submit" disabled={isSubmitting}>
+            {" "}
+            Cadastrar{" "}
+          </button>
+        </form>
       </Content>
     </Dialog.Portal>
   );
